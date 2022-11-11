83f3bea328edf80b512a8c11df7a10216183f439
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2022-11-11 22:20:32 +0700

    feat(components): main content

---
+++ components/Main.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+const Main = () => {
+  return (
+    <div className="w-full h-screen text-center">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <p className="uppercase text-sm tracking-widest text-gray-600">
+            Let&apos;s build something together
+          </p>
+          <h1 className="py-4 text-gray-700">
+            Hi, I&apos;m <span className="text-[#5651e5]">Agus</span>
+          </h1>
+          <h1 className="py-4 text-gray-700">A Fullstack Web Developer</h1>
+          <p className="py-4 text-gray-600 max-w-[70%] m-auto">
+            I&apos;m a fullstack web developer specializing in building (and
+            occasionally designing) exceptional digital experiences. Currently,
+            I&apos;m focused on building and developing backend rest api
+            applications while learning any technologies.
+          </p>
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaLinkedinIn />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaGithub />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <AiOutlineMail />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <BsFillPersonLinesFill />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '../components/Navbar'
+import Main from '../components/Main'
 
 export default function Home() {
   return (
@@ -12,6 +13,8 @@ export default function Home() {
       </Head>
 
       <Navbar />
+
+      <Main />
     </div>
   )
 }
