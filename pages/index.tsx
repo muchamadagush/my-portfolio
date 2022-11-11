1307776eac792976d4bd8a86689158cf1e96bd7c
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2022-11-12 00:31:51 +0700

    feat(components): contact

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Main from '../components/Main'
 import About from '../components/About'
 import Skills from '../components/Skills'
 import Projects from '../components/Projects'
+import Contact from '../components/Contact'
 
 export default function Home() {
   return (
@@ -24,6 +25,8 @@ export default function Home() {
       <Skills />
 
       <Projects />
+
+      <Contact />
     </div>
   )
 }
