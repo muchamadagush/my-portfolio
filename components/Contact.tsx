a136594805702019ef55407af36af9f8534e9dbb
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2022-11-13 01:43:06 +0700

    fix(page): padding top projects and contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -9,7 +9,7 @@ import Link from "next/link";
 const Contact = () => {
   return (
     <div id='contact' className="w-full lg:h-screen">
-      <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
+      <div className="max-w-[1240px] m-auto px-2 pb-16 pt-24 w-full">
         <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           Contact
         </p>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,7 +6,7 @@ import propertYImg from '../public/assets/Agus.png'
 const Projects = () => {
   return (
     <div id='projects' className='w-full'>
-      <div className='max-w-[1240px] mx-auto px-2 py-16'>
+      <div className='max-w-[1240px] mx-auto px-2 pb-16 pt-24'>
         <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Projects</p>
         <h2 className='py-4'>What I&apos;ve Built</h2>
         <div className='grid md:grid-cols-2 gap-8'>
