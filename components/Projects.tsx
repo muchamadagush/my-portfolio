90b2d123a83cc9086dab3a310fa75a647794ccbf
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2022-11-13 02:56:56 +0700

    fix(projects): display data projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,61 +1,54 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import propertYImg from '../public/assets/Agus.png'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Portfolio from "../public/assets/projects/web-portfolio.png";
 
 const Projects = () => {
+  const dataProjects = [
+    {
+      thumbnail: Portfolio,
+      title: "Web Portfolio",
+      mainTechnology: "Next JS",
+      slug: "web-portfolio"
+    },
+  ];
+
   return (
-    <div id='projects' className='w-full'>
-      <div className='max-w-[1240px] mx-auto px-2 pb-16 pt-24'>
-        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Projects</p>
-        <h2 className='py-4'>What I&apos;ve Built</h2>
-        <div className='grid md:grid-cols-2 gap-8'>
-
-          <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
-            <Image className='rounded-xl group-hover:opacity-10' src={propertYImg} alt={'/'} />
-            <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-              <h3 className='text-2xl text-white tracking-wider text-center'>Property Finder</h3>
-              <p className='pb-4 pt-2 text-white text-center'>Next JS</p>
-              <Link href={'/projects/project4'}>
-                <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
-              </Link>
-            </div>
-          </div>
-          <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
-            <Image className='rounded-xl group-hover:opacity-10' src={propertYImg} alt={'/'} />
-            <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-              <h3 className='text-2xl text-white tracking-wider text-center'>Property Finder</h3>
-              <p className='pb-4 pt-2 text-white text-center'>Next JS</p>
-              <Link href={'/projects/project3'}>
-                <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
-              </Link>
-            </div>
-          </div>
-          <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
-            <Image className='rounded-xl group-hover:opacity-10' src={propertYImg} alt={'/'} />
-            <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-              <h3 className='text-2xl text-white tracking-wider text-center'>Property Finder</h3>
-              <p className='pb-4 pt-2 text-white text-center'>Next JS</p>
-              <Link href={'/projects/project2'}>
-                <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
-              </Link>
-            </div>
-          </div>
-          <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
-            <Image className='rounded-xl group-hover:opacity-10' src={propertYImg} alt={'/'} />
-            <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-              <h3 className='text-2xl text-white tracking-wider text-center'>Property Finder</h3>
-              <p className='pb-4 pt-2 text-white text-center'>Next JS</p>
-              <Link href={'/projects/project1'}>
-                <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
-              </Link>
-            </div>
-          </div>
-
+    <div id="projects" className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 pb-16 pt-24">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          Projects
+        </p>
+        <h2 className="py-4">What I&apos;ve Built</h2>
+        <div className="grid md:grid-cols-2 gap-8">
+          {dataProjects &&
+            dataProjects.map((item: any, index: number) => (
+              <div
+                key={index}
+                className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]"
+              >
+                <Image
+                  className="rounded-xl group-hover:opacity-10"
+                  src={Portfolio}
+                  alt={"/"}
+                />
+                <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+                  <h3 className="text-2xl text-white tracking-wider text-center">
+                    {item.title}
+                  </h3>
+                  <p className="pb-4 pt-2 text-white text-center">{item.mainTechnology}</p>
+                  <Link href={`/projects/${item.slug}`}>
+                    <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
+                      More Info
+                    </p>
+                  </Link>
+                </div>
+              </div>
+            ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
